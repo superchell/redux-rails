ae69d4c6e48516a425ace83c5db98d16e3f89ea4
gena <acidcrash2005@gmail.com>
2018-06-12 11:28:43 +0300

    Server bug

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,41 +1,51 @@
-import React from 'react'
-import { connect, Provider } from 'react-redux'
-import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, StaticRouter } from 'react-router-dom'
 
-import { createStore, applyMiddleware } from 'redux'
-import createHistory from 'history/createBrowserHistory'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import { Link } from 'react-router-dom'
-import { Route, Switch } from 'react-router'
+import createHistory from "history/createBrowserHistory";
+import { Route } from "react-router";
+
+import {
+    ConnectedRouter,
+    routerReducer,
+    routerMiddleware,
+    push
+} from "react-router-redux";
+
+import reducers from "./reducers"; // Or wherever you keep your reducers
+
+import Home from './HelloWorld'
+import Items from './Items'
 
 
-export default (props) => {
-    const history = createHistory()
+export default (data) => {
+    // Create a history of your choosing (we're using a browser history in this case)
+    const history = createHistory();
 
+// Build the middleware for intercepting and dispatching navigation actions
+    const middleware = routerMiddleware(history);
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
     const store = createStore(
-        routerReducer,
-        applyMiddleware(routerMiddleware(history)),
-    )
-
-    const ConnectedSwitch = connect(state => ({
-        location: state.location
-    }))(Switch)
-
-    const AppContainer = () => (
-        <ConnectedSwitch>
-            <Route exact path="/" component={() => (<h1>Home <Link to="/about">About</Link></h1>)} />
-            <Route path="/about" component={() => (<h1>About <Link to="/">Home</Link></h1>)} />
-        </ConnectedSwitch>
-    )
-
-    const App = connect(state => ({
-        location: state.location,
-    }))(AppContainer)
+        combineReducers({
+            ...reducers,
+            router: routerReducer
+        }),
+        applyMiddleware(middleware)
+    );
 
     return(
         <Provider store={store}>
+            {/* ConnectedRouter will use the store from Provider automatically */}
             <ConnectedRouter history={history}>
-                <App />
+                <div>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/items" component={Items} />
+                </div>
             </ConnectedRouter>
         </Provider>
     )
